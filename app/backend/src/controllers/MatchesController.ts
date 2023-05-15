d2452bad774886c845d5d7716ebc796966cc8eef
GuilhermeFMP <guilhermemoraispinho@gmail.com>
2023-05-15 15:26:56 -0300

    adicionado o query do endpoint matches

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -4,6 +4,11 @@ import statusCode from '../utils/statusCode';
 
 class MatchesController {
   public static async getMatches(req: Request, res: Response): Promise<void | Response> {
+    const { inProgress } = req.query;
+    if (typeof inProgress === 'string') {
+      const matches = await MatchesService.getInProgress(inProgress);
+      return res.status(statusCode.oK).json(matches);
+    }
     const matches = await MatchesService.getMatches();
     return res.status(statusCode.oK).json(matches);
   }
--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -17,6 +17,24 @@ class MatchesService {
     );
     return matches;
   }
+
+  public static async getInProgress(inProgress: string | undefined) {
+    const allMatches = await Matches.findAll(
+      { where: { inProgress: inProgress === 'true' },
+        include: [{
+          model: Teams,
+          as: 'homeTeam',
+          attributes: { exclude: ['id'] },
+        },
+        {
+          model: Teams,
+          as: 'awayTeam',
+          attributes: { exclude: ['id'] },
+        }] },
+    );
+
+    return allMatches;
+  }
 }
 
 export default MatchesService;
